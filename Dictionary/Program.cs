0307d5433d6053c202bf23bcc91f709274ea7ad0
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2024-07-08 12:32:08 +0200

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Dictionary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Partendo da una lista di studenti crea un dictionary
+             * che abbia come chiave il nome e come valore una lista 
+             * di studenti con lo stesso nome
+            */
+
+            var studentList = new List<Student>
+            {
+                new Student("Andrea", "Rossi"),
+                new Student("Andrea", "Verdi"),
+                new Student("Pippo", "Pippi"),
+                new Student("Pluto", "Pluti"),
+                new Student("Topolino", "Topolinis"),
+                new Student("Pippo", "De Pippi")
+            };
+
+            var studentDictionary = new Dictionary<string, List<Student>>();
+
+            foreach (var s in studentList)
+            {
+                if (!studentDictionary.ContainsKey(s.Name))
+                {
+                    studentDictionary.Add(s.Name, new List<Student> { s });
+                }
+                else
+                    studentDictionary[s.Name].Add(s);
+            }
+
+            foreach (var v in studentDictionary.Values)
+            {
+                v.ForEach( s => 
+                    Console.WriteLine($"Nome: {s.Name}, Cognome: {s.Surname}")
+                    );
+            }
+        }
+    }
+}
---
+++ Dictionary/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public class Student
+    {
+        public string Name { get; set; }   
+        public string Surname { get; set; }
+
+        public Student(string n, string s)
+        {
+            Name = n;
+            Surname = s;
+        }
+    }
+}
